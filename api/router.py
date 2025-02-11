ae3f1bde9a25388670cedb461b7d0cc4ee241a3d
Michael Olaniyan <103427098+mikolanny@users.noreply.github.com>
2025-02-11 20:35:29 +0100

    Updated router.py

--- api/router.py
+++ api/router.py
@@ -5,3 +5,4 @@ from api.routes import books
 api_router = APIRouter()
 api_router.include_router(books.router, prefix="/books", tags=["books"])
 #api_router.include_router(book_router, prefix="/api/v1")
+
