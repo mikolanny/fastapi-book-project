dd8c0d327d5e982aa0217678c427e90f680ac329
mikolanny <olaniyanlanny@gmail.com>
2025-02-11 20:24:11 +0100

    fix: fixed traceback error in router file

--- api/router.py
+++ api/router.py
@@ -4,4 +4,5 @@ from api.routes import books
 
 api_router = APIRouter()
 api_router.include_router(books.router, prefix="/books", tags=["books"])
-app.include_router(book_router, prefix="/api/v1")
+#api_router.include(book_router, prefix="/api/v1")
+
