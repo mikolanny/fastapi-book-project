227ee95272dfbe3c87b791f261e78531a598c6e8
Michael Olaniyan <103427098+mikolanny@users.noreply.github.com>
2025-02-16 05:58:51 +0100

    fix

--- api/routes/books.py
+++ api/routes/books.py
@@ -75,7 +75,7 @@ async def get_book(book_id: int) -> Book:
     book = db.get_book(book_id)
     if not book:
         raise HTTPException(
-            status_code=status.HTTP_200_OK,
+            status_code=status.HTTP_404_NOT_FOUND,
             detail="Book not found"
         )
     return book
